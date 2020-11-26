7a527766e9a84c8a848b39af322445ab74570147
pilifjed <pilifjed@gmail.com>
2020-11-26 19:38:35 +0100

    added visualisation

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,6 +1,8 @@
 package app;
 
 import org.apache.log4j.BasicConfigurator;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
 
 import java.util.logging.Logger;
 
@@ -12,5 +14,15 @@ public class App {
         Parameters parameters = Parameters.readArgs(args);
 
         System.out.println(parameters);
+        Graph graph = new SingleGraph("Tutorial 1");
+
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addEdge("AB", "A", "B");
+        graph.addEdge("BC", "B", "C");
+        graph.addEdge("CA", "C", "A");
+
+        graph.display();
     }
 }
