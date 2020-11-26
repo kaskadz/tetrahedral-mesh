fb806b7182a770b81106b176c99fba35a9521386
Kasper <kasper.kadzielawa@gmail.com>
2020-11-26 18:33:26 +0100

    Add Initializer

---
+++ src/main/java/processing/Initializer.java
@@ -0,0 +1,9 @@
+package processing;
+
+import model.TetrahedralGraph;
+
+public class Initializer {
+    public TetrahedralGraph initializeGraph() {
+        throw new UnsupportedOperationException("TODO");
+    }
+}
