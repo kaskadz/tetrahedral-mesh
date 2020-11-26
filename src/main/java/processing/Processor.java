5fac3bb8e1c9a59a5c48846e50896c55ddf67077
Kasper <kasper.kadzielawa@gmail.com>
2020-11-26 18:31:24 +0100

    Add Processor

---
+++ src/main/java/processing/Processor.java
@@ -0,0 +1,35 @@
+package processing;
+
+import model.TetrahedralGraph;
+import production.Production;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Processor {
+    private final Map<Integer, Production> productionMap;
+
+    public Processor(List<Production> productions) {
+        this.productionMap = productions
+                .stream()
+                .collect(Collectors.toMap(
+                        Production::getProductionId,
+                        production -> production));
+    }
+
+    public TetrahedralGraph applyProductions(TetrahedralGraph graph, int level, int[] productionIds) {
+        List<Production> productionStream = Arrays.stream(productionIds)
+                .mapToObj(productionMap::get)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+
+        for (Production p : productionStream) {
+            graph = p.tryApply(graph, level);
+        }
+
+        return graph;
+    }
+}
