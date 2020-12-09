ee8a04ae20c545e8ccae86560b933d46a38bbd49
Kasper <kasper.kadzielawa@gmail.com>
2020-12-09 20:08:54 +0100

    Add CustomPredicates

---
+++ src/main/java/common/CustomPredicates.java
@@ -0,0 +1,13 @@
+package common;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class CustomPredicates {
+    public static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
+        Map<Object, Boolean> map = new ConcurrentHashMap<>();
+        return t -> map.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+    }
+}
