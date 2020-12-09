6b6b0847813d48b649a0619b87861f46379a7e93
Kasper <kasper.kadzielawa@gmail.com>
2020-12-09 20:59:38 +0100

    Add ui.class and ui.stylesheet attribute names

--- src/main/java/common/Attributes.java
+++ src/main/java/common/Attributes.java
@@ -3,6 +3,8 @@ package common;
 public class Attributes {
     public static final String XY = "xy";
     public static final String LABEL = "ui.label";
+    public static final String CLASS = "ui.class";
+    public static final String STYLESHEET = "ui.stylesheet";
     public static final String FROZEN_LAYOUT = "layout.frozen";
 
     public static final String LEVEL = "custom.node.level";
