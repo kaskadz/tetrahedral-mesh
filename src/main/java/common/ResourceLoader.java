6d16a8f994c1777152a7261159d90cdec3f24b91
Kasper <kasper.kadzielawa@gmail.com>
2020-12-10 18:36:14 +0100

    Add interior node icons

--- src/main/java/common/ResourceLoader.java
+++ src/main/java/common/ResourceLoader.java
@@ -4,9 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.stream.Collectors;
 
-public class StylesheetReader {
+public class ResourceLoader {
     public static String readStyleSheet() {
         InputStream stream = ClassLoader.getSystemResourceAsStream("styles.css");
         if (stream == null) {
@@ -19,5 +20,27 @@ public class StylesheetReader {
         } catch (IOException e) {
             throw new RuntimeException("Reading the styles failed", e);
         }
+    }
+
+    public static URL getUpIconPath() {
+        return getIconUrlOrThrow("triangle_up.png");
+    }
+
+    public static URL getDownIconPath() {
+        return getIconUrlOrThrow("triangle_down.png");
+    }
+
+    public static URL getUpDownIconPath() {
+        return getIconUrlOrThrow("triangle_updown.png");
+    }
+
+    private static URL getIconUrlOrThrow(String iconFileName) {
+        URL iconPathUrl = ClassLoader.getSystemResource(String.format("icons/%s", iconFileName));
+
+        if (iconPathUrl == null) {
+            throw new RuntimeException(String.format("Couldn't find the up icon file: %s", iconFileName));
+        }
+
+        return iconPathUrl;
     }
 }
