fbfb7d0cf60abe5a0db323c71764637d907a41bd
PawelGedlek <pawelgedlek@gmail.com>
2020-12-22 12:53:09 +0100

    tests refactor

--- src/test/java/production/Production6Tests.java
+++ src/test/java/production/Production6Tests.java
@@ -2,116 +2,74 @@ package production;
 
 import common.CustomCollectors;
 import common.ProductionApplicationException;
-import model.GraphNode;
 import model.InteriorNode;
-import model.Point2d;
 import model.TetrahedralGraph;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import utils.TestGraphs;
 
 import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Production6Tests extends AbstractProductionTest {
+    private final static Production production6 = new Production6();
+
     @Test
     public void shouldHaveProperNumber() {
-        Production sut = new Production6();
-
-        assertEquals(6, sut.getProductionId());
+        // then
+        assertEquals(6, production6.getProductionId());
     }
 
     @Test
     public void shouldApplyOnCorrectLeftSide() {
-        Production sut = new Production6();
-
-        TetrahedralGraph correctGraph = getGraph(false, false);
+        // given
+        TetrahedralGraph correctGraph = TestGraphs.getProduction6CorrectLeftSide();
         InteriorNode interiorNode = correctGraph.getInteriorNodes().stream().collect(CustomCollectors.toSingle());
 
-        Executable production = () -> sut.apply(correctGraph, interiorNode, Collections.emptyList());
+        // when
+        Executable production = () -> production6.apply(correctGraph, interiorNode, Collections.emptyList());
 
+        // then
         assertDoesNotThrow(production);
     }
 
     @Test
-    public void shouldApplyOnCorrectLeftSideWithAdditionalNodes() {
-        Production sut = new Production6();
-
-        TetrahedralGraph correctGraph = getGraph(true, false);
+    public void shouldApplyOnCorrectLeftSideWithAdditionalNodesOnRight() {
+        // given
+        TetrahedralGraph correctGraph = TestGraphs.getProduction6CorrectLeftSideWithAdditionalNodesOnRight();
         InteriorNode interiorNode = correctGraph.getInteriorNodes().stream().collect(CustomCollectors.toSingle());
 
-        System.out.println(interiorNode);
+        // when
+        Executable production = () -> production6.apply(correctGraph, interiorNode, Collections.emptyList());
 
-        Executable production = () -> sut.apply(correctGraph, interiorNode, Collections.emptyList());
-
+        // then
         assertDoesNotThrow(production);
     }
 
     @Test
-    public void shouldNotApplyOnIncorrectLeftSide() {
-        Production sut = new Production6();
-
-        TetrahedralGraph incorrectGraph = getGraph(false, true);
+    public void shouldNotApplyOnLeftSideWithBrokenLink() {
+        // given
+        TetrahedralGraph incorrectGraph = TestGraphs.getProduction6LeftSideWithBrokenLink();
         InteriorNode interiorNode = incorrectGraph.getInteriorNodes().stream().collect(CustomCollectors.toSingle());
 
-        System.out.println(interiorNode);
+        // when
+        Executable exception = () -> production6.apply(incorrectGraph, interiorNode, Collections.emptyList());
 
-        Executable production = () -> sut.apply(incorrectGraph, interiorNode, Collections.emptyList());
-
-        assertThrows(ProductionApplicationException.class, production);
+        // then
+        assertThrows(ProductionApplicationException.class, exception);
     }
 
-    private TetrahedralGraph getGraph(boolean additionalNodes, boolean breakGraph) {
-        int graphLevel = 0;
+    @Test
+    public void shouldNotApplyOnLeftSideWithMissingNode() {
+        // given
+        TetrahedralGraph incorrectGraph = TestGraphs.getProduction6LeftSideWithMissingNode();
+        InteriorNode interiorNode = incorrectGraph.getInteriorNodes().stream().collect(CustomCollectors.toSingle());
 
-        TetrahedralGraph graph = new TetrahedralGraph();
+        // when
+        Executable exception = () -> production6.apply(incorrectGraph, interiorNode, Collections.emptyList());
 
-        InteriorNode center = graph.insertInteriorNode(graphLevel, "I");
-
-        GraphNode bottomLeft = graph.insertGraphNode(graphLevel, "E", new Point2d(-1, -1));
-        GraphNode midLeft = graph.insertGraphNode(graphLevel, "E", new Point2d(-1, 0));
-        GraphNode topLeft = graph.insertGraphNode(graphLevel, "E", new Point2d(-1, 1));
-        GraphNode midTop = graph.insertGraphNode(graphLevel, "E", new Point2d(0, 1));
-        GraphNode topRight = graph.insertGraphNode(graphLevel, "E", new Point2d(1, 1));
-        GraphNode midRight = graph.insertGraphNode(graphLevel, "E", new Point2d(1, 0));
-        GraphNode bottomRight = graph.insertGraphNode(graphLevel, "E", new Point2d(1, -1));
-        GraphNode midBottom = graph.insertGraphNode(graphLevel, "E", new Point2d(0, -1));
-
-        graph.connectNodes(center, topLeft);
-        graph.connectNodes(center, topRight);
-        graph.connectNodes(center, bottomLeft);
-        graph.connectNodes(center, bottomRight);
-
-        graph.connectNodes(topLeft, midLeft);
-        graph.connectNodes(topLeft, midTop);
-
-        graph.connectNodes(topRight, midRight);
-        if (breakGraph) {
-            GraphNode between = graph.insertGraphNode(graphLevel, "E", new Point2d(0.5, 1));
-
-            graph.connectNodes(midTop, between);
-            graph.connectNodes(between, topRight);
-        } else {
-            graph.connectNodes(topRight, midTop);
-        }
-
-        graph.connectNodes(bottomRight, midRight);
-        graph.connectNodes(bottomRight, midBottom);
-
-        graph.connectNodes(bottomLeft, midLeft);
-        graph.connectNodes(bottomLeft, midBottom);
-
-        // Inserts 3 additional nodes, after this, production still should be applicable.
-        if (additionalNodes) {
-            GraphNode rightTop = graph.insertGraphNode(graphLevel, "E", new Point2d(2, 1));
-            GraphNode rightCenter = graph.insertGraphNode(graphLevel, "E", new Point2d(2, 0));
-            GraphNode rightBottom = graph.insertGraphNode(graphLevel, "E", new Point2d(2, -1));
-
-            graph.connectNodes(topRight, rightTop);
-            graph.connectNodes(midRight, rightCenter);
-            graph.connectNodes(bottomRight, rightBottom);
-        }
-
-        return graph;
+        // then
+        assertThrows(ProductionApplicationException.class, exception);
     }
 }
---
+++ src/test/java/utils/TestGraphs.java
@@ -0,0 +1,190 @@
+package utils;
+
+import model.GraphNode;
+import model.InteriorNode;
+import model.Point2d;
+import model.TetrahedralGraph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TestGraphs {
+    public static TetrahedralGraph getProduction6CorrectLeftSide() {
+        Map<Integer, Pair<Float>> nodesMap = new HashMap<>();
+
+        // nodesMap.put(0, new Pair<>(0.0f, 0.0f)); // center
+        nodesMap.put(1, new Pair<>(-1.0f, -1.0f)); // bottomLeft
+        nodesMap.put(2, new Pair<>(-1.0f, 0.0f)); // midLeft
+        nodesMap.put(3, new Pair<>(-1.0f, 1.0f)); // topLeft
+        nodesMap.put(4, new Pair<>(0.0f, 1.0f)); // midTop
+        nodesMap.put(5, new Pair<>(1.0f, 1.0f)); // topRight
+        nodesMap.put(6, new Pair<>(1.0f, 0.0f)); // midRight
+        nodesMap.put(7, new Pair<>(1.0f, -1.0f)); // bottomRight
+        nodesMap.put(8, new Pair<>(0.0f, -1.0f)); // midBottom
+
+        List<Pair<Integer>> links = new ArrayList<>();
+
+        links.add(new Pair<>(0, 3));
+        links.add(new Pair<>(0, 5));
+        links.add(new Pair<>(0, 1));
+        links.add(new Pair<>(0, 7));
+        links.add(new Pair<>(3, 2));
+        links.add(new Pair<>(3, 4));
+        links.add(new Pair<>(5, 6));
+        links.add(new Pair<>(5, 4));
+        links.add(new Pair<>(7, 6));
+        links.add(new Pair<>(7, 8));
+        links.add(new Pair<>(1, 2));
+        links.add(new Pair<>(1, 8));
+
+        return prepareGraph(nodesMap, links);
+    }
+
+    public static TetrahedralGraph getProduction6CorrectLeftSideWithAdditionalNodesOnRight() {
+        Map<Integer, Pair<Float>> nodesMap = new HashMap<>();
+
+        // nodesMap.put(0, new Pair<>(0.0f, 0.0f)); // center
+        nodesMap.put(1, new Pair<>(-1.0f, -1.0f)); // bottomLeft
+        nodesMap.put(2, new Pair<>(-1.0f, 0.0f)); // midLeft
+        nodesMap.put(3, new Pair<>(-1.0f, 1.0f)); // topLeft
+        nodesMap.put(4, new Pair<>(0.0f, 1.0f)); // midTop
+        nodesMap.put(5, new Pair<>(1.0f, 1.0f)); // topRight
+        nodesMap.put(6, new Pair<>(1.0f, 0.0f)); // midRight
+        nodesMap.put(7, new Pair<>(1.0f, -1.0f)); // bottomRight
+        nodesMap.put(8, new Pair<>(0.0f, -1.0f)); // midBottom
+        // additional nodes
+        nodesMap.put(9, new Pair<>(2.0f, 1.0f)); // rightTop
+        nodesMap.put(10, new Pair<>(2.0f, 0.0f)); // rightCenter
+        nodesMap.put(11, new Pair<>(2.0f, -1.0f)); // rightBottom
+
+        List<Pair<Integer>> links = new ArrayList<>();
+
+        links.add(new Pair<>(0, 3));
+        links.add(new Pair<>(0, 5));
+        links.add(new Pair<>(0, 1));
+        links.add(new Pair<>(0, 7));
+        links.add(new Pair<>(3, 2));
+        links.add(new Pair<>(3, 4));
+        links.add(new Pair<>(5, 6));
+        links.add(new Pair<>(5, 4));
+        links.add(new Pair<>(7, 6));
+        links.add(new Pair<>(7, 8));
+        links.add(new Pair<>(1, 2));
+        links.add(new Pair<>(1, 8));
+        // additional links
+        links.add(new Pair<>(5, 9));
+        links.add(new Pair<>(6, 10));
+        links.add(new Pair<>(7, 11));
+
+        return prepareGraph(nodesMap, links);
+    }
+
+    public static TetrahedralGraph getProduction6LeftSideWithBrokenLink() {
+        Map<Integer, Pair<Float>> nodesMap = new HashMap<>();
+
+        // nodesMap.put(0, new Pair<>(0.0f, 0.0f)); // center
+        nodesMap.put(1, new Pair<>(-1.0f, -1.0f)); // bottomLeft
+        nodesMap.put(2, new Pair<>(-1.0f, 0.0f)); // midLeft
+        nodesMap.put(3, new Pair<>(-1.0f, 1.0f)); // topLeft
+        nodesMap.put(4, new Pair<>(0.0f, 1.0f)); // midTop
+        nodesMap.put(5, new Pair<>(1.0f, 1.0f)); // topRight
+        nodesMap.put(6, new Pair<>(1.0f, 0.0f)); // midRight
+        nodesMap.put(7, new Pair<>(1.0f, -1.0f)); // bottomRight
+        nodesMap.put(8, new Pair<>(0.0f, -1.0f)); // midBottom
+        // break node
+        nodesMap.put(9, new Pair<>(0.5f, 1.0f)); // between
+
+        List<Pair<Integer>> links = new ArrayList<>();
+
+        links.add(new Pair<>(0, 3));
+        links.add(new Pair<>(0, 5));
+        links.add(new Pair<>(0, 1));
+        links.add(new Pair<>(0, 7));
+        links.add(new Pair<>(3, 2));
+        links.add(new Pair<>(3, 4));
+        links.add(new Pair<>(5, 6));
+        links.add(new Pair<>(7, 6));
+        links.add(new Pair<>(7, 8));
+        links.add(new Pair<>(1, 2));
+        links.add(new Pair<>(1, 8));
+        // break link
+        links.add(new Pair<>(4, 9));
+        links.add(new Pair<>(9, 5));
+
+        return prepareGraph(nodesMap, links);
+    }
+
+    public static TetrahedralGraph getProduction6LeftSideWithMissingNode() {
+        Map<Integer, Pair<Float>> nodesMap = new HashMap<>();
+
+        // nodesMap.put(0, new Pair<>(0.0f, 0.0f)); // center
+        nodesMap.put(1, new Pair<>(-1.0f, -1.0f)); // bottomLeft
+        nodesMap.put(2, new Pair<>(-1.0f, 0.0f)); // midLeft
+        nodesMap.put(3, new Pair<>(-1.0f, 1.0f)); // topLeft
+        nodesMap.put(4, new Pair<>(0.0f, 1.0f)); // midTop
+        nodesMap.put(5, new Pair<>(1.0f, 1.0f)); // topRight
+        // nodesMap.put(6, new Pair<>(1.0f, 0.0f)); // midRight
+        nodesMap.put(7, new Pair<>(1.0f, -1.0f)); // bottomRight
+        nodesMap.put(8, new Pair<>(0.0f, -1.0f)); // midBottom
+
+        List<Pair<Integer>> links = new ArrayList<>();
+
+        links.add(new Pair<>(0, 3));
+        links.add(new Pair<>(0, 5));
+        links.add(new Pair<>(0, 1));
+        links.add(new Pair<>(0, 7));
+        links.add(new Pair<>(3, 2));
+        links.add(new Pair<>(3, 4));
+        links.add(new Pair<>(5, 7));
+        links.add(new Pair<>(7, 8));
+        links.add(new Pair<>(1, 2));
+        links.add(new Pair<>(1, 8));
+
+        return prepareGraph(nodesMap, links);
+    }
+
+    private static TetrahedralGraph prepareGraph(Map<Integer, Pair<Float>> nodesCoords, List<Pair<Integer>> links) {
+        int graphLevel = 0;
+
+        TetrahedralGraph graph = new TetrahedralGraph();
+
+        InteriorNode center = graph.insertInteriorNode(graphLevel, "I");
+        Map<Integer, GraphNode> nodesMap = new HashMap<>();
+
+        nodesCoords.forEach((key, value) -> {
+            GraphNode newNode = graph.insertGraphNode(graphLevel, "E", new Point2d(value.getLeft(), value.getRight()));
+            nodesMap.put(key, newNode);
+        });
+
+        // connect with center
+        links.stream().filter(link -> link.getLeft() == 0)
+                .forEach(link -> graph.connectNodes(center, nodesMap.get(link.getRight())));
+
+        // connect with other nodes
+        links.stream().filter(link -> link.getLeft() != 0)
+                .forEach(link -> graph.connectNodes(nodesMap.get(link.getLeft()), nodesMap.get(link.getRight())));
+
+        return graph;
+    }
+
+    private static class Pair<T> {
+        private T left;
+        private T right;
+
+        public Pair(T left, T right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        public T getLeft() {
+            return left;
+        }
+
+        public T getRight() {
+            return right;
+        }
+    }
+
+}
