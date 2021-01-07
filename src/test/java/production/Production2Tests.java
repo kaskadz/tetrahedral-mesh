414733ad43b194513a4f4d427f970424621ee80b
Kasper <kasper.kadzielawa@gmail.com>
2021-01-07 21:05:46 +0100

    Improve Production2Tests

--- src/test/java/production/Production2Tests.java
+++ src/test/java/production/Production2Tests.java
@@ -5,14 +5,17 @@ import common.ProductionApplicationException;
 import initialization.EntrySymbolInitializer;
 import model.GraphNode;
 import model.InteriorNode;
+import model.Point2d;
 import model.TetrahedralGraph;
-import org.javatuples.Triplet;
+import org.javatuples.Quartet;
 import org.javatuples.Tuple;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import visualization.MultiLevelVisualizer;
+import visualization.MultiStepMultiLevelVisualizer;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -26,6 +29,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class Production2Tests extends AbstractProductionTest {
     private final Production sut = new Production2();
+    private final boolean visualMode = false;
 
     @Test
     public void shouldHaveProperNumber() {
@@ -33,16 +37,23 @@ public class Production2Tests extends AbstractProductionTest {
         assertEquals(2, sut.getProductionId());
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{0}")
     @MethodSource("validGraphs")
     public void shouldApplyProductionIfApplicable(
-            TetrahedralGraph graph, InteriorNode interiorNode, List<GraphNode> graphNodes) {
+            String testCaseName, TetrahedralGraph graph, InteriorNode interiorNode, List<GraphNode> graphNodes) {
+        // Arrange
+        MultiStepMultiLevelVisualizer visualizer = new MultiStepMultiLevelVisualizer();
+        visualizer.addStep(graph);
+
         // Act
         Executable production = () -> sut.apply(graph, interiorNode, graphNodes);
 
         // Assert
         assertDoesNotThrow(production);
+        visualizer.addStep(graph);
+
         assertChildGraphIsCreatedProperly(interiorNode);
+        if (visualMode) visualizer.displayAll(testCaseName);
     }
 
     private void assertChildGraphIsCreatedProperly(InteriorNode interiorNode) {
@@ -55,10 +66,14 @@ public class Production2Tests extends AbstractProductionTest {
         assertThat(graphNodeChildren).hasSize(9);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{0}")
     @MethodSource("invalidGraphs")
     public void shouldThrowExceptionIfProductionIsNotApplicable(
-            TetrahedralGraph graph, InteriorNode interiorNode, List<GraphNode> graphNodes) {
+            String testCaseName, TetrahedralGraph graph, InteriorNode interiorNode, List<GraphNode> graphNodes) {
+        // Arrange
+        MultiLevelVisualizer visualizer = new MultiLevelVisualizer();
+        if (visualMode) visualizer.displayGraph(graph, testCaseName);
+
         // Act
         Executable production = () -> sut.apply(graph, interiorNode, graphNodes);
 
@@ -67,7 +82,7 @@ public class Production2Tests extends AbstractProductionTest {
     }
 
     public static Stream<Arguments> invalidGraphs() {
-        List<Triplet<TetrahedralGraph, InteriorNode, List<GraphNode>>> invalidGraphs = new ArrayList<>();
+        List<Quartet<String, TetrahedralGraph, InteriorNode, List<GraphNode>>> invalidGraphs = new ArrayList<>();
 
         EntrySymbolInitializer entrySymbolInitializer = new EntrySymbolInitializer();
 
@@ -77,34 +92,65 @@ public class Production2Tests extends AbstractProductionTest {
 
         // Graph 1
         graph = entrySymbolInitializer.initializeGraph();
-        invalidGraphs.add(new Triplet<>(graph, null, graphNodes));
+        invalidGraphs.add(new Quartet<>("Interior node is null", graph, null, graphNodes));
 
         // Graph 2
         graph = entrySymbolInitializer.initializeGraph();
         InteriorNode interiorNode1 = graph.getInteriorNodes().stream().collect(CustomCollectors.toSingle());
         new Production1().apply(graph, interiorNode1, Collections.emptyList());
         interiorNode = interiorNode1.getChildren().collect(CustomCollectors.toSingle());
-        invalidGraphs.add(new Triplet<>(graph, interiorNode, graph.getGraphNodes().stream().limit(3).collect(Collectors.toList())));
+        invalidGraphs.add(new Quartet<>("GraphNodeList is not empty", graph, interiorNode,
+                graph.getGraphNodes().stream().limit(3).collect(Collectors.toList())));
 
         // Graph 3
-        graph = entrySymbolInitializer.initializeGraph();
+        graph = new TetrahedralGraph();
+        graph.insertInteriorNode(0, "i");
         interiorNode = graph.getInteriorNodes().stream().collect(CustomCollectors.toSingle());
-        invalidGraphs.add(new Triplet<>(graph, interiorNode, graphNodes));
+        invalidGraphs.add(new Quartet<>("Application on small 'i'", graph, interiorNode, graphNodes));
 
         // Graph 4
         graph = entrySymbolInitializer.initializeGraph();
         interiorNode1 = graph.getInteriorNodes().stream().collect(CustomCollectors.toSingle());
         new Production1().apply(graph, interiorNode1, Collections.emptyList());
         interiorNode = interiorNode1.getChildren().collect(CustomCollectors.toSingle());
-        interiorNode.getSiblingsIds().findFirst().ifPresent(graph::removeGraphNode);
-        invalidGraphs.add(new Triplet<>(graph, interiorNode, graphNodes));
+        graph.removeGraphNode(interiorNode.getSiblingsIds().findFirst().get());
+        invalidGraphs.add(new Quartet<>("Missing interior's sibling", graph, interiorNode, graphNodes));
 
+        // Graph 5
+        graph = entrySymbolInitializer.initializeGraph();
+        interiorNode1 = graph.getInteriorNodes().stream().collect(CustomCollectors.toSingle());
+        new Production1().apply(graph, interiorNode1, Collections.emptyList());
+        interiorNode = interiorNode1.getChildren().collect(CustomCollectors.toSingle());
+        GraphNode graphNode = interiorNode.getSiblings().findFirst().get();
+        String sibling1Id = graphNode.getId();
+        String sibling2Id = graphNode.getSiblingsIds().findFirst().get();
+        graph.getGraph().removeEdge(sibling1Id, sibling2Id);
+        invalidGraphs.add(new Quartet<>("Missing edge between interior's siblings", graph, interiorNode, graphNodes));
+
+        // Graph 6
+        graph = entrySymbolInitializer.initializeGraph();
+        interiorNode1 = graph.getInteriorNodes().stream().collect(CustomCollectors.toSingle());
+        new Production1().apply(graph, interiorNode1, Collections.emptyList());
+        interiorNode = interiorNode1.getChildren().collect(CustomCollectors.toSingle());
+        graphNode = interiorNode.getSiblings().findFirst().get();
+        Set<GraphNode> siblings = graphNode.getSiblings().collect(Collectors.toSet());
+        Point2d coordinates = graphNode.getCoordinates();
+        coordinates = new Point2d(coordinates.getX() * 2.0, coordinates.getY() * 2);
+        int level = graphNode.getLevel();
+        String symbol = graphNode.getSymbol();
+        graph.removeGraphNode(graphNode);
+        graphNode = graph.insertGraphNode(level, symbol, coordinates);
+        for (GraphNode gn : siblings) {
+            graph.connectNodes(graphNode, gn);
+        }
+        graph.connectNodes(interiorNode, graphNode);
+        invalidGraphs.add(new Quartet<>("Misplaced interior's sibling", graph, interiorNode, graphNodes));
 
         return invalidGraphs.stream().map(Tuple::toArray).map(Arguments::of);
     }
 
     public static Stream<Arguments> validGraphs() {
-        List<Triplet<TetrahedralGraph, InteriorNode, List<GraphNode>>> validGraphs = new ArrayList<>();
+        List<Quartet<String, TetrahedralGraph, InteriorNode, List<GraphNode>>> validGraphs = new ArrayList<>();
 
         TetrahedralGraph graph;
         InteriorNode interiorNode;
@@ -117,7 +163,7 @@ public class Production2Tests extends AbstractProductionTest {
         InteriorNode interiorNode1 = graph.getInteriorNodes().stream().collect(CustomCollectors.toSingle());
         new Production1().apply(graph, interiorNode1, Collections.emptyList());
         interiorNode = interiorNode1.getChildren().collect(CustomCollectors.toSingle());
-        validGraphs.add(new Triplet<>(graph, interiorNode, graphNodes));
+        validGraphs.add(new Quartet<>("P1 -> (P2)", graph, interiorNode, graphNodes));
 
         // Graph 2
         graph = entrySymbolInitializer.initializeGraph();
@@ -126,7 +172,7 @@ public class Production2Tests extends AbstractProductionTest {
         interiorNode1 = interiorNode1.getChildren().collect(CustomCollectors.toSingle());
         new Production2().apply(graph, interiorNode1, Collections.emptyList());
         interiorNode = interiorNode1.getChildren().findFirst().get();
-        validGraphs.add(new Triplet<>(graph, interiorNode, graphNodes));
+        validGraphs.add(new Quartet<>("P1 -> P2 -> (P2)", graph, interiorNode, graphNodes));
 
         return validGraphs.stream().map(Tuple::toArray).map(Arguments::of);
     }
