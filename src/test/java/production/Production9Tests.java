6186631c865204ace430640cd8c7c660874d9686
Koozco <kussy.kusy@gmail.com>
2021-01-19 14:15:53 +0100

    Add complex test. Fix wrong tests caused by error in pdf images

--- src/test/java/production/Production9Tests.java
+++ src/test/java/production/Production9Tests.java
@@ -13,7 +13,9 @@ import org.junit.jupiter.api.function.Executable;
 import visualization.MultiStepMultiLevelVisualizer;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,6 +23,108 @@ public class Production9Tests extends AbstractProductionTest {
 
     private final MultiStepMultiLevelVisualizer visualizer = new MultiStepMultiLevelVisualizer();
     private final static boolean visualMode = false;
+
+    public static Comparator<GraphNode> byX = Comparator.comparing(
+            x -> x.getCoordinates().getX()
+    );
+
+    public static Comparator<GraphNode> byY = Comparator.comparing(
+            x -> x.getCoordinates().getY()
+    );
+
+    public static List<GraphNode> getNodesForProd7(int startingNode, List<GraphNode> sortedNodes) {
+        List<GraphNode> nodes = sortedNodes
+                .get(startingNode)
+                .getSiblings()
+                .filter(x -> x.getCoordinates().getX() == 0.0 || x.getCoordinates().getY() == 0.0)
+                .collect(Collectors.toList());
+
+        nodes.add(sortedNodes.get(startingNode));
+        nodes.sort(byX.thenComparing(byY));
+
+        return nodes;
+    }
+
+    @Test
+    public void shouldApplyNestedInComplexGraph() {
+        // Arrange
+        TetrahedralGraph graph = new TetrahedralGraph();
+        List<GraphNode> graphNodes = Collections.emptyList();
+        Production prod1 = new Production1();
+        Production prod2 = new Production2();
+        Production prod7 = new Production7();
+        Production prod = new Production9();
+
+        // Level 0
+        InteriorNode initialNode = graph.insertInteriorNode(0, "E");
+        visualizer.addStep(graph);
+
+        // Level 1
+        prod1.apply(graph, initialNode, graphNodes);
+        visualizer.addStep(graph);
+
+        // Level 2
+        InteriorNode interiorNode1 = initialNode.getChildren().collect(CustomCollectors.toSingle());
+        prod2.apply(graph, interiorNode1, graphNodes);
+        visualizer.addStep(graph);
+
+        // Level 3
+        interiorNode1.getChildren().forEach(nodeLevel2 -> {
+                    prod2.apply(graph, nodeLevel2, graphNodes);
+                }
+        );
+        visualizer.addStep(graph);
+
+        //Merging on level 3
+        List<GraphNode> sortedNodes = graph.getGraphNodesByLevel(graph.getMaxLevel())
+                .stream()
+                .filter(x -> (x.getCoordinates().getX() == 0.0 || x.getCoordinates().getY() == 0.0)
+                        && (Math.abs(x.getCoordinates().getX()) == 0.5 || Math.abs(x.getCoordinates().getY()) == 0.5))
+                .sorted(byX.thenComparing(byY))
+                .collect(Collectors.toList());
+
+
+        for (int i = 0; i < 3; i++) {
+            List<GraphNode> nodes = getNodesForProd7(i * 2, sortedNodes);
+            prod7.apply(graph, null, nodes);
+
+        }
+        visualizer.addStep(graph);
+
+        // Missing prod 12
+        // Now we should apply prod 12 to merge nodes on segment (0.0, 0.0), (1.0, 0.0)
+        // Will do it "manually"
+        List<GraphNode> toBeMerged = graph.getGraphNodesByLevel(graph.getMaxLevel())
+                .stream()
+                .filter(
+                        x -> x.getCoordinates().getX() == 1.0 && x.getCoordinates().getY() == 0.0
+                )
+                .collect(Collectors.toList());
+
+        graph.mergeNodes(toBeMerged.get(0), toBeMerged.get(1));
+        visualizer.addStep(graph);
+
+        List<GraphNode> forProd9 = graph.getGraphNodesByLevel(graph.getMaxLevel())
+                .stream()
+                .filter(
+                        x -> x.getCoordinates().getX() >= 0.0 && x.getCoordinates().getY() == 0.0
+                )
+                .collect(Collectors.toList());
+
+//        for (GraphNode node : forProd9) {
+//            System.out.println("" + node.getCoordinates().getX() + " " + node.getCoordinates().getY());
+//        }
+
+        // Act
+        Executable subject = () -> prod.apply(graph, null, forProd9);
+
+        // Assert
+        assertDoesNotThrow(subject);
+        visualizer.addStep(graph);
+
+        // Visualize
+        if (visualMode) visualizer.displayAll("ComplexGraph");
+    }
 
     @Test
     public void shouldHaveProperNumber() {
@@ -51,7 +155,7 @@ public class Production9Tests extends AbstractProductionTest {
         TetrahedralGraph graph = new TetrahedralGraph();
 
         // Level 0
-        GraphNode initialNode = graph.insertGraphNode(0, "e", new Point2d(0, 0));
+        GraphNode initialNode = graph.insertGraphNode(1, "e", new Point2d(0, 0));
         InteriorNode leftI = graph.insertInteriorNode(1, "i");
         graph.connectNodes(leftI, initialNode);
         InteriorNode rightI = graph.insertInteriorNode(1, "i");
@@ -92,7 +196,7 @@ public class Production9Tests extends AbstractProductionTest {
         visualizer.addStep(graph);
 
         Production prod = new Production9();
-        List<GraphNode> nodes = Lists.newArrayList(initialNode, e1, e2, e3, e4);
+        List<GraphNode> nodes = Lists.newArrayList(e1, e2, e3, e4);
 
         // Act
         Executable subject = () -> prod.apply(graph, null, nodes);
@@ -111,7 +215,7 @@ public class Production9Tests extends AbstractProductionTest {
         TetrahedralGraph graph = new TetrahedralGraph();
 
         // Level 0
-        GraphNode initialNode = graph.insertGraphNode(0, "e", new Point2d(0, 0));
+        GraphNode initialNode = graph.insertGraphNode(1, "e", new Point2d(0, 0));
         InteriorNode leftI = graph.insertInteriorNode(1, "i");
         graph.connectNodes(leftI, initialNode);
         InteriorNode rightI = graph.insertInteriorNode(1, "i");
@@ -151,7 +255,7 @@ public class Production9Tests extends AbstractProductionTest {
 
 
         Production prod = new Production9();
-        List<GraphNode> nodes = Lists.newArrayList(initialNode, e1, e2, e3, e4);
+        List<GraphNode> nodes = Lists.newArrayList(e1, e2, e3, e4);
 
         // Act
         Executable subject = () -> prod.apply(graph, null, nodes);
@@ -166,7 +270,7 @@ public class Production9Tests extends AbstractProductionTest {
         TetrahedralGraph graph = new TetrahedralGraph();
 
         // Level 0
-        GraphNode initialNode = graph.insertGraphNode(0, "e", new Point2d(0, 0));
+        GraphNode initialNode = graph.insertGraphNode(1, "e", new Point2d(0, 0));
         InteriorNode leftI = graph.insertInteriorNode(1, "i");
         graph.connectNodes(leftI, initialNode);
         InteriorNode rightI = graph.insertInteriorNode(1, "i");
@@ -206,7 +310,7 @@ public class Production9Tests extends AbstractProductionTest {
 
 
         Production prod = new Production9();
-        List<GraphNode> nodes = Lists.newArrayList(initialNode, e1, e2, e3, e4);
+        List<GraphNode> nodes = Lists.newArrayList(e1, e2, e3, e4);
 
         // Act
         Executable subject = () -> prod.apply(graph, null, nodes);
@@ -221,7 +325,7 @@ public class Production9Tests extends AbstractProductionTest {
         TetrahedralGraph graph = new TetrahedralGraph();
 
         // Level 0
-        GraphNode initialNode = graph.insertGraphNode(0, "e", new Point2d(0, 0));
+        GraphNode initialNode = graph.insertGraphNode(1, "e", new Point2d(0, 0));
         InteriorNode leftI = graph.insertInteriorNode(1, "i");
         graph.connectNodes(leftI, initialNode);
         InteriorNode rightI = graph.insertInteriorNode(1, "i");
@@ -257,7 +361,7 @@ public class Production9Tests extends AbstractProductionTest {
 
 
         Production prod = new Production9();
-        List<GraphNode> nodes = Lists.newArrayList(initialNode, e1, e2, e3, e4);
+        List<GraphNode> nodes = Lists.newArrayList(e1, e2, e3, e4);
 
         // Act
         Executable subject = () -> prod.apply(graph, null, nodes);
@@ -272,7 +376,7 @@ public class Production9Tests extends AbstractProductionTest {
         TetrahedralGraph graph = new TetrahedralGraph();
 
         // Level 0
-        GraphNode initialNode = graph.insertGraphNode(0, "e", new Point2d(0, 0));
+        GraphNode initialNode = graph.insertGraphNode(1, "e", new Point2d(0, 0));
         InteriorNode leftI = graph.insertInteriorNode(1, "i");
         graph.connectNodes(leftI, initialNode);
         InteriorNode rightI = graph.insertInteriorNode(1, "i");
@@ -313,7 +417,7 @@ public class Production9Tests extends AbstractProductionTest {
 
 
         Production prod = new Production9();
-        List<GraphNode> nodes = Lists.newArrayList(initialNode, e1, e2, e3, e4);
+        List<GraphNode> nodes = Lists.newArrayList(e1, e2, e3, e4);
 
         // Act
         Executable subject = () -> prod.apply(graph, null, nodes);
